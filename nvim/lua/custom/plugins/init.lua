ec7181656e306cc7e9fbfa3962a4c584978cde7b
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-06-14 11:39:52 +0000

    indentation
    to add indentations

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,27 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+
+    config = function()
+      local highlight = {
+        'CursorColumn',
+        'Whitespace',
+      }
+      require('ibl').setup {
+        indent = { highlight = highlight, char = '' },
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = false,
+        },
+        scope = { enabled = false },
+      }
+    end,
+  },
+}
