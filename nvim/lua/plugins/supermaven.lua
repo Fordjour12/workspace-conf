cbe337d738079afd66eec69ef19ce344c875b7de
Bobie Fordjour Kofi McCamble <53586559+Fordjour12@users.noreply.github.com>
2024-10-09 06:21:44 +0000

    supermaven.lua extension

---
+++ nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "nvim-cmp",
+    dependencies = {
+      {
+        "supermaven-inc/supermaven-nvim",
+        opts = {},
+        config = function(_, opts)
+          require("supermaven-nvim").setup({
+            keymaps = {
+
+              accept_suggestion = "<Tab>",
+              clear_suggestion = "<C-]>",
+              accept_word = "<C-j>",
+            },
+            color = {
+              suggestion_color = "#87B573",
+              cterm = 244,
+            },
+            log_level = "info", -- set to "off" to disable logging completely
+            disable_inline_completion = false, -- disables inline completion for use with cmp
+            disable_keymaps = true, -- disables built in keymaps for more manual control
+            condition = function()
+              return false
+            end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+          })
+        end,
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "supermaven",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+}
